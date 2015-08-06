c35a752acea64865a3db59af9fc68d0d302658de
ferschke <ferschke@cs.cmu.edu>
2015-08-06 17:23:18 -0400

    Started integrating DiscourseDB access layer

--- discoursedb-io-edx/discoursedb-io-edx/src/main/java/edu/cmu/cs/lti/discoursedb/io/edx/forum/converter/EdxForumConverter.java
+++ discoursedb-io-edx/discoursedb-io-edx/src/main/java/edu/cmu/cs/lti/discoursedb/io/edx/forum/converter/EdxForumConverter.java
@@ -5,32 +5,61 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Iterator;
-import org.apache.logging.log4j.Logger;
+
 import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import edu.cmu.cs.lti.discoursedb.core.model.macro.Discourse;
+import edu.cmu.cs.lti.discoursedb.core.repository.macro.ContentRepository;
+import edu.cmu.cs.lti.discoursedb.core.repository.macro.ContributionRepository;
+import edu.cmu.cs.lti.discoursedb.core.repository.macro.DiscoursePartRepository;
+import edu.cmu.cs.lti.discoursedb.core.repository.macro.DiscourseRepository;
+import edu.cmu.cs.lti.discoursedb.core.repository.user.UserRepository;
 import edu.cmu.cs.lti.discoursedb.io.edx.forum.model.Post;
 
 public class EdxForumConverter {
 
 	private static final Logger logger = LogManager.getLogger(EdxForumConverter.class);
+
 	private static int postcount = 1;
-	
-	public static void main(String[] args) throws Exception{
+
+	/*
+	 * Entity-Repositories for DiscourseDB connection.
+	 */
+
+	@Autowired
+	private UserRepository userRepo;
+
+	@Autowired
+	private DiscourseRepository discourseRepo;
+
+	@Autowired
+	private ContributionRepository contributionRepo;
+
+	@Autowired
+	private ContentRepository contentRepo;
+
+	@Autowired
+	private DiscoursePartRepository discoursePartRepo;
+
+	public static void main(String[] args) throws Exception {
 		EdxForumConverter converter = new EdxForumConverter();
-		if(args.length!=1){
+		if (args.length != 1) {
 			logger.error("Missing input file. Must provide input file as launch parameter.");
 			System.exit(1);
 		}
 		String inFileName = args[0];
-		
+
 		File infile = new File(inFileName);
-		if(!infile.exists()||!infile.isFile()||!infile.canRead()){
+		if (!infile.exists() || !infile.isFile() || !infile.canRead()) {
 			logger.error("Input file does not exist or is not readable.");
-			System.exit(1);			
+			System.exit(1);
 		}
 		logger.trace("Starting forum conversion");
 		converter.convert(inFileName);
@@ -40,15 +69,19 @@ public class EdxForumConverter {
 	 * Stream-reads the json input and binds each post in the dataset to an
 	 * object that is passed on to the mapper.
 	 * 
-	 * @param filename of json file that contains forum data 
-	 * @throws IOException in case the inFile could not be read
-	 * @throws JsonParseException in case the json was malformed and couln't be parsed 
+	 * @param filename
+	 *            of json file that contains forum data
+	 * @throws IOException
+	 *             in case the inFile could not be read
+	 * @throws JsonParseException
+	 *             in case the json was malformed and couln't be parsed
 	 */
-	private void convert(String inFile) throws JsonParseException, JsonProcessingException, IOException {
+	public void convert(String inFile) throws JsonParseException, JsonProcessingException, IOException {
 		final InputStream in = new FileInputStream(inFile);
 		try {
-			for (Iterator<Post> it = new ObjectMapper().readValues(new JsonFactory().createParser(in),Post.class); it.hasNext();){
-				logger.debug("Retrieving post number "+postcount++);
+			for (Iterator<Post> it = new ObjectMapper().readValues(new JsonFactory().createParser(in), Post.class); it
+					.hasNext();) {
+				logger.debug("Retrieving post number " + postcount++);
 				Post curPost = it.next();
 				map(curPost);
 			}
@@ -56,15 +89,45 @@ public class EdxForumConverter {
 			in.close();
 		}
 	}
-	
+
 	/**
 	 * Maps a post to DiscourseDB entities.
 	 * 
 	 * @param p
+	 *            the post object to map to DiscourseDB
 	 */
-	private void map(Post p){
-		logger.trace("Mapping post "+p.getId());
-//		System.out.println(p);
+	public void map(Post p) {
+		logger.trace("Mapping post " + p.getId());
+
+		// ---------- Init Discourse -----------
+		logger.trace("Init Discourse entity");
+
+		// edX course ids are unique, but in DiscourseDB the combination of
+		// discourse name and descriptor are unique.
+		// So, we use the course id both as name and descriptor.
+		String courseid = p.getCourseId();
+		Discourse curDiscourse = discourseRepo.findOneByNameAndDescriptor(courseid, courseid);
+		if (curDiscourse == null) {
+			curDiscourse = new Discourse(courseid, courseid);
+			discourseRepo.save(curDiscourse);
+		}
+
+		// ---------- Init DiscoursePart -----------
+		logger.trace("Init DiscoursePart entity");
+
+		// ---------- Init User -----------
+		logger.trace("Init User entity");
+
+		// ---------- Create Content -----------
+		logger.trace("Create Content entity");
+
+		// ---------- Create Contribution -----------
+		logger.trace("Create Contribution entity");
+
+		// TODO represent other properties
+
+		
+		logger.trace("Post mapping completed.");
 	}
 
 }
