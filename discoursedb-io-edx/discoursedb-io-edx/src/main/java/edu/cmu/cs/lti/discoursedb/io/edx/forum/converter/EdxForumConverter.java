f6031bc823fdaa12df626d515757aae3542a4ff5
ferschke <ferschke@cs.cmu.edu>
2015-08-06 16:47:17 -0400

    Implemented converter framework. Added logging.

--- discoursedb-io-edx/discoursedb-io-edx/src/main/java/edu/cmu/cs/lti/discoursedb/io/edx/forum/converter/EdxForumConverter.java
+++ discoursedb-io-edx/discoursedb-io-edx/src/main/java/edu/cmu/cs/lti/discoursedb/io/edx/forum/converter/EdxForumConverter.java
@@ -1,33 +1,70 @@
 package edu.cmu.cs.lti.discoursedb.io.edx.forum.converter;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Iterator;
-
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import edu.cmu.cs.lti.discoursedb.io.edx.forum.model.Post;
 
 public class EdxForumConverter {
 
+	private static final Logger logger = LogManager.getLogger(EdxForumConverter.class);
+	private static int postcount = 1;
+	
 	public static void main(String[] args) throws Exception{
 		EdxForumConverter converter = new EdxForumConverter();
-//		converter.convert(args[0]);
-		converter.convert("/usr0/home/oliverf/dalmoocdata/forum.json");
+		if(args.length!=1){
+			logger.error("Missing input file. Must provide input file as launch parameter.");
+			System.exit(1);
+		}
+		String inFileName = args[0];
+		
+		File infile = new File(inFileName);
+		if(!infile.exists()||!infile.isFile()||!infile.canRead()){
+			logger.error("Input file does not exist or is not readable.");
+			System.exit(1);			
+		}
+		logger.trace("Starting forum conversion");
+		converter.convert(inFileName);
 	}
 
-	private void convert(String inFile) throws Exception{
+	/**
+	 * Stream-reads the json input and binds each post in the dataset to an
+	 * object that is passed on to the mapper.
+	 * 
+	 * @param filename of json file that contains forum data 
+	 * @throws IOException in case the inFile could not be read
+	 * @throws JsonParseException in case the json was malformed and couln't be parsed 
+	 */
+	private void convert(String inFile) throws JsonParseException, JsonProcessingException, IOException {
 		final InputStream in = new FileInputStream(inFile);
 		try {
 			for (Iterator<Post> it = new ObjectMapper().readValues(new JsonFactory().createParser(in),Post.class); it.hasNext();){
+				logger.debug("Retrieving post number "+postcount++);
 				Post curPost = it.next();
-
-				System.out.println(curPost);				
+				map(curPost);
 			}
 		} finally {
 			in.close();
 		}
+	}
+	
+	/**
+	 * Maps a post to DiscourseDB entities.
+	 * 
+	 * @param p
+	 */
+	private void map(Post p){
+		logger.trace("Mapping post "+p.getId());
+//		System.out.println(p);
 	}
 
 }
