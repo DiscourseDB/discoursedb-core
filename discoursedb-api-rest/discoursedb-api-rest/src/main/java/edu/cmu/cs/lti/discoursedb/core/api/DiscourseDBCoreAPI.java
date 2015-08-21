51d6a4ab8df2ae37a499e3b8634e2faf6b97693b
ferschke <ferschke@cs.cmu.edu>
2015-08-21 16:22:02 -0400

    Improved WebService config

--- discoursedb-api-rest/discoursedb-api-rest/src/main/java/edu/cmu/cs/lti/discoursedb/core/api/DiscourseDBCoreAPI.java
+++ discoursedb-api-rest/discoursedb-api-rest/src/main/java/edu/cmu/cs/lti/discoursedb/core/api/DiscourseDBCoreAPI.java
@@ -1,14 +1,42 @@
 package edu.cmu.cs.lti.discoursedb.core.api;
 
+import javax.servlet.ServletRegistration;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import edu.cmu.cs.lti.discoursedb.configuration.BaseConfiguration;
+import edu.cmu.cs.lti.discoursedb.configuration.WebConfiguration;
 
 @SpringBootApplication
 @ComponentScan(value = { "edu.cmu.cs.lti.discoursedb" })
-public class DiscourseDBCoreAPI {
-    public static void main(String[] args) {
-        SpringApplication.run(DiscourseDBCoreAPI.class, args);
-    }
+public class DiscourseDBCoreAPI extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscourseDBCoreAPI.class, args);
+	}
+
+	@Override
+	protected void customizeRegistration(ServletRegistration.Dynamic registration) {
+		registration.setInitParameter("dispatchOptionsRequest", "true");
+		registration.setAsyncSupported(true);
+	}
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class<?>[] { BaseConfiguration.class };
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] { WebConfiguration.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
 
 }
---
+++ discoursedb-api-rest/discoursedb-api-rest/src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <link rel="stylesheet" href="./libs/bootstrap-css-only/css/bootstrap.min.css" />
+  </head>
+    <script type="text/javascript" src="./libs/angular/angular.min.js"></script>
+    <script type="text/javascript" src="./libs/angular-resource/angular-resource.min.js"></script>
+    <script type="text/javascript" src="./libs/angular-spring-data-rest/dist/angular-spring-data-rest.min.js"></script>
+    <script type="text/javascript" src="./libs/lodash/dist/lodash.min.js"></script>
+  </body>
+</html>
