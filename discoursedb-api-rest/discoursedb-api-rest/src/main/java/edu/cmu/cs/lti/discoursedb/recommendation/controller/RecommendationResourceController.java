917d5144dbfd913ea0a61bf46f36030ed6ab82cb
ferschke <ferschke@cs.cmu.edu>
2015-09-11 14:21:25 -0400

    Recommendation HATEOAS dummy classes

---
+++ discoursedb-api-rest/discoursedb-api-rest/src/main/java/edu/cmu/cs/lti/discoursedb/recommendation/controller/RecommendationResourceController.java
@@ -0,0 +1,10 @@
+package edu.cmu.cs.lti.discoursedb.recommendation.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/recommendationdata")
+public class RecommendationResourceController {
+
+}
---
+++ discoursedb-api-rest/discoursedb-api-rest/src/main/java/edu/cmu/cs/lti/discoursedb/recommendation/resource/RecommendationResource.java
@@ -0,0 +1,8 @@
+package edu.cmu.cs.lti.discoursedb.recommendation.resource;
+
+import org.springframework.hateoas.ResourceSupport;
+
+public class RecommendationResource extends ResourceSupport{
+
+	
+}
