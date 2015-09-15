fe375ff16ce62e9b230b1fdd77c6b89be7bf31bd
ferschke <ferschke@cs.cmu.edu>
2015-09-15 16:58:21 -0400

    Updated RecommendationUserResource

--- discoursedb-api-rest/discoursedb-api-rest/src/main/java/edu/cmu/cs/lti/discoursedb/api/recommendation/resource/RecommendationUserResource.java
+++ discoursedb-api-rest/discoursedb-api-rest/src/main/java/edu/cmu/cs/lti/discoursedb/api/recommendation/resource/RecommendationUserResource.java
@@ -8,10 +8,40 @@ public class RecommendationUserResource extends ResourceSupport {
 	
 	private String username;
 	private String realname;
+	private String email;
+	private String edxId;
+	private String country;
 	
 	public RecommendationUserResource(User user) {
 		this.setUsername(user.getUsername());
 		this.setRealname(user.getRealname());
+		this.setEdxId(user.getSourceId());
+		this.setEmail(user.getEmail());
+		this.setCountry(user.getCountry());
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getEdxId() {
+		return edxId;
+	}
+
+	public void setEdxId(String edxId) {
+		this.edxId = edxId;
 	}
 
 	public String getUsername() {
