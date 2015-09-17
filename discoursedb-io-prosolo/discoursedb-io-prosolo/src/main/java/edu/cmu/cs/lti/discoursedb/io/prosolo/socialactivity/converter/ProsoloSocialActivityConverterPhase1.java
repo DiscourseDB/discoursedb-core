639ced8ba963c1049a84fb60bd10ccba2239813c
ferschke <ferschke@cs.cmu.edu>
2015-09-17 15:36:57 -0400

    Stub for database-to-database conversion

--- discoursedb-io-prosolo/discoursedb-io-prosolo/src/main/java/edu/cmu/cs/lti/discoursedb/io/prosolo/socialactivity/converter/ProsoloSocialActivityConverterPhase1.java
+++ discoursedb-io-prosolo/discoursedb-io-prosolo/src/main/java/edu/cmu/cs/lti/discoursedb/io/prosolo/socialactivity/converter/ProsoloSocialActivityConverterPhase1.java
@@ -3,33 +3,32 @@ package edu.cmu.cs.lti.discoursedb.io.prosolo.socialactivity.converter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.transaction.Transactional;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.jooq.lambda.SQL;
+import org.jooq.lambda.Unchecked;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
+import edu.cmu.cs.lti.discoursedb.io.prosolo.socialactivity.model.SocialActivity;
+
 /**
- * This converter loads the forum json file specified in the arguments of the
- * app and parses the json into Post objects and maps each post object to
- * DiscourseDB.
+ * This converter loads data from a prosolo database and maps it to DiscourseDB.
+ * The DiscourseDB configuration is defined in the dicoursedb-model project and
+ * Spring/Hibernate are taking care of connections.
  * 
- * Many of the relations between entities are actually modeled in the form of
- * relation tables which allows us to keep track of the time window in which the
- * relation was active. However, this also entails that we need to explicitly
- * instantiate these relations - i.e. we have to create a "relationship-entity"
- * which makes the code more verbose.
+ * The connection to the prosolo database is more lightweight and uses a JDBC
+ * connection. The configuration parameters for this connection are passed to
+ * the converter as launch parameters in the following order
  * 
- * The conversion is split into three phases. Phase 1 (this class) imports all
- * of the data except for the DiscoursRelations. These relations are created
- * between entities and require the entities to be present in the database. That
- * is why they are created in a second pass (Phase2) Phase 3 adds personal
- * information about the user to the database that comes from a different file.
+ * <prosolo_dbhost> <prosolo_db> <prosolo_dbuser> <prosolo_dbpwd>
  * 
  * @author Oliver Ferschke
  *
@@ -42,84 +41,168 @@ public class ProsoloSocialActivityConverterPhase1 implements CommandLineRunner {
 	private static final Logger logger = LogManager.getLogger(ProsoloSocialActivityConverterPhase1.class);
 
 	private Connection con = null;
+	private String host;
+	private String db;
+	private String user;
+	private String pwd;
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+
 		if (args.length != 4) {
 			logger.error("Missing database credentials <prosolo_dbhost> <prosolo_db> <prosolo_dbuser> <prosolo_dbpwd>");
 			System.exit(1);
 		}
-		
-		///////////////////////////////////////////////
-		
-		logger.info("Establishing connection to prosolo database...");
-		try {
-			con = getConnection(args[0], args[1], args[2], args[3]);
-		} catch (SQLException ex) {
-			logger.error(ex.getMessage(), ex);
-			System.exit(1);
-		}
-		logger.info("...established");
 
-		///////////////////////////////////////////////	
-		
+		this.host = args[0];
+		this.db = args[1];
+		this.user = args[2];
+		this.pwd = args[3];
+
 		logger.info("Start mapping to DiscourseDB...");
 		try {
 			map();
 		} catch (SQLException ex) {
 			logger.error(ex.getMessage(), ex);
 		} finally {
-			try {
-				if (con != null) {
-					con.close();
-				}
-			} catch (SQLException ex) {
-				logger.error(ex.getMessage(), ex);
-			}
+			closeConnection();
+
 		}
 		logger.info("...mapping complete");
-	
+
 		///////////////////////////////////////////////
 	}
-	
 
+	/**
+	 * Maps prosolo data to DiscourseDB.
+	 * 
+	 * Within this method, only statements need to be properly closed. The
+	 * connection is managed by the the calling run method and will be closed
+	 * eventually.
+	 * 
+	 * @throws SQLException
+	 */
 	private void map() throws SQLException {
-		PreparedStatement pst = con.prepareStatement("SELECT * FROM user");
-		pst.setFetchSize(Integer.MIN_VALUE);
-		// pst.setString(1, author);
-		
-		ResultSet res = pst.executeQuery();
-		try{
-			while (res.next()) {
-				System.out.println(res.getString("lastname"));
-			}			
-		}finally{
-			res.close();
-			pst.close();
+		mapPostSocialActivities();
+		//TODO mapNodeSocialAcitvities
+		//TODO apXSocialActivities
+	}
+
+	
+	private void mapPostSocialActivities() throws SQLException{
+		List<Long> ids = getIdsForDtype("PostSocialActivity");
+		for (Long l : ids) {
+			SocialActivity curACtivity = getSocialActivity(l);
+			
+			//TODO do something with the activity
+			//at this point, we are free to make new queries using any properties in the activity object			
+		}
+	}
+	
+	/**
+	 * Returns all ids for social activities of the given dtype. This idlist can
+	 * then be used to query one social activity at a time. This might be
+	 * preferred to streaming a whole resultset (even though this would have a
+	 * higher performance), because no other statements can be executed before
+	 * the (streaming) query isn't closed.
+	 * 
+	 * @param dtype
+	 *            the dtype for the social activity
+	 * @return a list of ids for social activities of the provided dtype
+	 * @throws SQLException
+	 */
+	private List<Long> getIdsForDtype(String dtype) throws SQLException {
+		List<Long> idList = null;
+
+		String sql = "SELECT id from social_activity where dtype=?";
+		try (PreparedStatement stmt = getConnection().prepareStatement(sql)) {
+			stmt.setString(1, dtype);
+			idList = SQL.seq(stmt, Unchecked.function(rs -> rs.getLong("id"))).collect(Collectors.toList());
+		}
+		return idList;
+	}
+
+	/**
+	 * Returns a single SocialActivity object
+	 * 
+	 * @param id
+	 *            the id of the social activity object
+	 * @return a list of ids for social activities of the provided dtype
+	 * @throws SQLException
+	 */
+	private SocialActivity getSocialActivity(Long id) throws SQLException {
+		SocialActivity activity = null;
+		try (Connection c = getConnection()) {
+			String sql = "SELECT * from social_activity where id=?";
+			try (PreparedStatement stmt = c.prepareStatement(sql)) {
+				stmt.setLong(1, id); 
+				activity = SQL.seq(stmt, Unchecked.function(rs -> new SocialActivity(
+						rs.getString("dtype"),
+						rs.getLong("id"),
+						rs.getString("created"),
+						rs.getString("deleted"),
+						rs.getString("dc_description"),
+						rs.getString("title"),
+						rs.getString("action"),
+						rs.getLong("bookmark_count"),
+						rs.getString("comments_disabled"),
+						rs.getInt("dislike_count"),
+						rs.getString("last_action"),
+						rs.getInt("like_count"),
+						rs.getInt("share_count"),
+						rs.getString("text"),
+						rs.getString("visibility"),
+						rs.getString("avatar_url"),
+						rs.getString("name"),
+						rs.getString("nickname"),
+						rs.getString("post_link"),
+						rs.getString("profile_url"),
+						rs.getInt("service_type"),
+						rs.getInt("user_type"),
+						rs.getLong("actor"),
+						rs.getLong("maker"),
+						rs.getLong("reason"),
+						rs.getLong("rich_content"),
+						rs.getLong("goal_target"),
+						rs.getLong("post_object"),
+						rs.getLong("user_target"),
+						rs.getLong("node_object"),
+						rs.getLong("user_object"),
+						rs.getLong("node_target"),
+						rs.getLong("node"),
+						rs.getLong("social_activity"),
+						rs.getLong("enrollment_object"),
+						rs.getLong("course_object"),
+						rs.getLong("course_enrollment_object")					
+		            ))).findAny().get();
+			}
+		}
+		return activity;
+	}
+
+	private Connection getConnection() throws SQLException {
+		if (con == null || con.isClosed()) {
+			return DriverManager.getConnection("jdbc:mysql://" + this.host + ":3306/" + this.db, this.user, this.pwd);
+		} else {
+			return con;
 		}
 	}
 
-/*
- * EXAMPLE FOR STREAMING A LARGE RESULT SET
- * 
- *	PreparedStatement pst = con.prepareStatement("SELECT * FROM user");
- *	pst.setFetchSize(Integer.MIN_VALUE);
- *	// pst.setString(1, author);
- *	
- *	ResultSet res = pst.executeQuery();
- *	try{
- *		while (res.next()) {
- *			System.out.println(res.getString("lastname"));
- *		}			
- *	}finally{
- *		res.close();
- *		pst.close();
- *	}
- */
-		
-	private static Connection getConnection(String host, String db, String user, String pwd) throws SQLException {
-		return DriverManager.getConnection("jdbc:mysql://" + host + ":3306/" + db, user, pwd);
+	private void closeConnection() throws SQLException {
+		if (con != null && !con.isClosed()) {
+			con.close();
+		}
 	}
-
+	
+	/*
+	 * EXAMPLE FOR STREAMING A LARGE RESULT SET
+	 * 
+	 * PreparedStatement pst = con.prepareStatement("SELECT * FROM user"
+	 * ,java.sql.ResultSet.TYPE_FORWARD_ONLY,java.sql.ResultSet.CONCUR_READ_ONLY
+	 * ); pst.setFetchSize(Integer.MIN_VALUE); // pst.setString(1, author);
+	 * 
+	 * ResultSet res = pst.executeQuery(); try{ while (res.next()) {
+	 * System.out.println(res.getString("lastname")); } }finally{ res.close();
+	 * pst.close(); }
+	 */
 }
--- discoursedb-io-prosolo/discoursedb-io-prosolo/src/main/java/edu/cmu/cs/lti/discoursedb/io/prosolo/socialactivity/model/SocialActivity.java
+++ discoursedb-io-prosolo/discoursedb-io-prosolo/src/main/java/edu/cmu/cs/lti/discoursedb/io/prosolo/socialactivity/model/SocialActivity.java
@@ -6,9 +6,6 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
  * Wraps entities from the social_activity table in prosolo. 
  * Comments are based on discussions with the prosolo developers.
  * 
- * Currently, all fields have String types but could be changed to more appropriate
- * types with the necessary datatype conversion.
- * 
  * @author Oliver Ferschke
  */
 @JsonPropertyOrder({ "dtype", "id", "created", "deleted", "dc_description", "title", "action", "bookmark_count",
@@ -18,11 +15,59 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 		"node_target", "node", "social_activity", "enrollment_object", "course_object", "course_enrollment_object" })
 public class SocialActivity {
 
+	
+
+	public SocialActivity(String dtype, Long id, String created, String deleted, String dc_description, String title,
+			String action, Long bookmark_count, String comments_disabled, Integer dislike_count, String last_action,
+			Integer like_count, Integer share_count, String text, String visibility, String avatar_url, String name,
+			String nickname, String post_link, String profile_url, Integer service_type, Integer user_type, Long actor,
+			Long maker, Long reason, Long rich_content, Long goal_target, Long post_object, Long user_target,
+			Long node_object, Long user_object, Long node_target, Long node, Long social_activity,
+			Long enrollment_object, Long course_object, Long course_enrollment_object) {
+		super();
+		this.dtype = dtype;
+		this.id = id;
+		this.created = created;
+		this.deleted = deleted;
+		this.dc_description = dc_description;
+		this.title = title;
+		this.action = action;
+		this.bookmark_count = bookmark_count;
+		this.comments_disabled = comments_disabled;
+		this.dislike_count = dislike_count;
+		this.last_action = last_action;
+		this.like_count = like_count;
+		this.share_count = share_count;
+		this.text = text;
+		this.visibility = visibility;
+		this.avatar_url = avatar_url;
+		this.name = name;
+		this.nickname = nickname;
+		this.post_link = post_link;
+		this.profile_url = profile_url;
+		this.service_type = service_type;
+		this.user_type = user_type;
+		this.actor = actor;
+		this.maker = maker;
+		this.reason = reason;
+		this.rich_content = rich_content;
+		this.goal_target = goal_target;
+		this.post_object = post_object;
+		this.user_target = user_target;
+		this.node_object = node_object;
+		this.user_object = user_object;
+		this.node_target = node_target;
+		this.node = node;
+		this.social_activity = social_activity;
+		this.enrollment_object = enrollment_object;
+		this.course_object = course_object;
+		this.course_enrollment_object = course_enrollment_object;
+	}
 	/**
 	 * Type of activity
 	 */
 	private String dtype;
-	private String id;
+	private Long id;
 	private String created;
 	/**
 	 * Whether activity has been deleted or not
@@ -38,15 +83,15 @@ public class SocialActivity {
 	 * etc.
 	 */
 	private String action;
-	private String bookmark_count;
+	private Long bookmark_count;
 	private String comments_disabled;
-	private String dislike_count;
+	private Integer dislike_count;
 	/**
 	 * Date when the social activity was carried out
 	 */
 	private String last_action;
-	private String like_count;
-	private String share_count;
+	private Integer like_count;
+	private Integer share_count;
 	/**
 	 * Textual content of the activity
 	 */
@@ -75,349 +120,277 @@ public class SocialActivity {
 	/**
 	 * Related to TwitterPostSocialActivity. Null for all other activities.
 	 */
-	private String service_type;
+	private Integer service_type;
 	/**
 	 * Twitter user or Prosolo user
 	 */
-	private String user_type;
+	private Integer user_type;
 	/**
 	 * Removed in new schema
 	 */
-	private String actor;
+	private Long actor;
 	/**
 	 * The user who performed the action in this activity
 	 */
-	private String maker;
+	private Long maker;
 	/**
 	 * Relation to another social activity
 	 */
-	private String reason;
-	private String rich_content;
+	private Long reason;
+	private Long rich_content;
 	/**
 	 * Pointer to goal note activity for which this action has been performed
 	 * Null for other types of activities.
 	 */
-	private String goal_target;
+	private Long goal_target;
 	/**
 	 * Pointer to post activityfor which this action has been performed Null for
 	 * other types of activities.
 	 */
-	private String post_object;
+	private Long post_object;
 	/**
 	 * Pointer to user activity for which this action has been performed. Null
 	 * for other types of activities.
 	 */
-	private String user_target;
-	private String node_object;
-	private String user_object;
+	private Long user_target;
+	private Long node_object;
+	private Long user_object;
 	/**
 	 * Pointer to node activity for which this action has been performed. Null
 	 * for other types of activities.
 	 */
-	private String node_target;
-	private String node;
+	private Long node_target;
+	private Long node;
 	/**
 	 * Relation to another social activity
 	 */
-	private String social_activity;
-	private String enrollment_object;
-	private String course_object;
-	private String course_enrollment_object;
-
+	private Long social_activity;
+	private Long enrollment_object;
+	private Long course_object;
+	private Long course_enrollment_object;
+	
 	public String getDtype() {
 		return dtype;
 	}
-
 	public void setDtype(String dtype) {
 		this.dtype = dtype;
 	}
-
-	public String getId() {
+	public Long getId() {
 		return id;
 	}
-
-	public void setId(String id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
-
 	public String getCreated() {
 		return created;
 	}
-
 	public void setCreated(String created) {
 		this.created = created;
 	}
-
 	public String getDeleted() {
 		return deleted;
 	}
-
 	public void setDeleted(String deleted) {
 		this.deleted = deleted;
 	}
-
 	public String getDc_description() {
 		return dc_description;
 	}
-
 	public void setDc_description(String dc_description) {
 		this.dc_description = dc_description;
 	}
-
 	public String getTitle() {
 		return title;
 	}
-
 	public void setTitle(String title) {
 		this.title = title;
 	}
-
 	public String getAction() {
 		return action;
 	}
-
 	public void setAction(String action) {
 		this.action = action;
 	}
-
-	public String getBookmark_count() {
+	public Long getBookmark_count() {
 		return bookmark_count;
 	}
-
-	public void setBookmark_count(String bookmark_count) {
+	public void setBookmark_count(Long bookmark_count) {
 		this.bookmark_count = bookmark_count;
 	}
-
 	public String getComments_disabled() {
 		return comments_disabled;
 	}
-
 	public void setComments_disabled(String comments_disabled) {
 		this.comments_disabled = comments_disabled;
 	}
-
-	public String getDislike_count() {
+	public Integer getDislike_count() {
 		return dislike_count;
 	}
-
-	public void setDislike_count(String dislike_count) {
+	public void setDislike_count(Integer dislike_count) {
 		this.dislike_count = dislike_count;
 	}
-
 	public String getLast_action() {
 		return last_action;
 	}
-
 	public void setLast_action(String last_action) {
 		this.last_action = last_action;
 	}
-
-	public String getLike_count() {
+	public Integer getLike_count() {
 		return like_count;
 	}
-
-	public void setLike_count(String like_count) {
+	public void setLike_count(Integer like_count) {
 		this.like_count = like_count;
 	}
-
-	public String getShare_count() {
+	public Integer getShare_count() {
 		return share_count;
 	}
-
-	public void setShare_count(String share_count) {
+	public void setShare_count(Integer share_count) {
 		this.share_count = share_count;
 	}
-
 	public String getText() {
 		return text;
 	}
-
 	public void setText(String text) {
 		this.text = text;
 	}
-
 	public String getVisibility() {
 		return visibility;
 	}
-
 	public void setVisibility(String visibility) {
 		this.visibility = visibility;
 	}
-
 	public String getAvatar_url() {
 		return avatar_url;
 	}
-
 	public void setAvatar_url(String avatar_url) {
 		this.avatar_url = avatar_url;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public String getNickname() {
 		return nickname;
 	}
-
 	public void setNickname(String nickname) {
 		this.nickname = nickname;
 	}
-
 	public String getPost_link() {
 		return post_link;
 	}
-
 	public void setPost_link(String post_link) {
 		this.post_link = post_link;
 	}
-
 	public String getProfile_url() {
 		return profile_url;
 	}
-
 	public void setProfile_url(String profile_url) {
 		this.profile_url = profile_url;
 	}
-
-	public String getService_type() {
+	public Integer getService_type() {
 		return service_type;
 	}
-
-	public void setService_type(String service_type) {
+	public void setService_type(Integer service_type) {
 		this.service_type = service_type;
 	}
-
-	public String getUser_type() {
+	public Integer getUser_type() {
 		return user_type;
 	}
-
-	public void setUser_type(String user_type) {
+	public void setUser_type(Integer user_type) {
 		this.user_type = user_type;
 	}
-
-	public String getActor() {
+	public Long getActor() {
 		return actor;
 	}
-
-	public void setActor(String actor) {
+	public void setActor(Long actor) {
 		this.actor = actor;
 	}
-
-	public String getMaker() {
+	public Long getMaker() {
 		return maker;
 	}
-
-	public void setMaker(String maker) {
+	public void setMaker(Long maker) {
 		this.maker = maker;
 	}
-
-	public String getReason() {
+	public Long getReason() {
 		return reason;
 	}
-
-	public void setReason(String reason) {
+	public void setReason(Long reason) {
 		this.reason = reason;
 	}
-
-	public String getRich_content() {
+	public Long getRich_content() {
 		return rich_content;
 	}
-
-	public void setRich_content(String rich_content) {
+	public void setRich_content(Long rich_content) {
 		this.rich_content = rich_content;
 	}
-
-	public String getGoal_target() {
+	public Long getGoal_target() {
 		return goal_target;
 	}
-
-	public void setGoal_target(String goal_target) {
+	public void setGoal_target(Long goal_target) {
 		this.goal_target = goal_target;
 	}
-
-	public String getPost_object() {
+	public Long getPost_object() {
 		return post_object;
 	}
-
-	public void setPost_object(String post_object) {
+	public void setPost_object(Long post_object) {
 		this.post_object = post_object;
 	}
-
-	public String getUser_target() {
+	public Long getUser_target() {
 		return user_target;
 	}
-
-	public void setUser_target(String user_target) {
+	public void setUser_target(Long user_target) {
 		this.user_target = user_target;
 	}
-
-	public String getNode_object() {
+	public Long getNode_object() {
 		return node_object;
 	}
-
-	public void setNode_object(String node_object) {
+	public void setNode_object(Long node_object) {
 		this.node_object = node_object;
 	}
-
-	public String getUser_object() {
+	public Long getUser_object() {
 		return user_object;
 	}
-
-	public void setUser_object(String user_object) {
+	public void setUser_object(Long user_object) {
 		this.user_object = user_object;
 	}
-
-	public String getNode_target() {
+	public Long getNode_target() {
 		return node_target;
 	}
-
-	public void setNode_target(String node_target) {
+	public void setNode_target(Long node_target) {
 		this.node_target = node_target;
 	}
-
-	public String getNode() {
+	public Long getNode() {
 		return node;
 	}
-
-	public void setNode(String node) {
+	public void setNode(Long node) {
 		this.node = node;
 	}
-
-	public String getSocial_activity() {
+	public Long getSocial_activity() {
 		return social_activity;
 	}
-
-	public void setSocial_activity(String social_activity) {
+	public void setSocial_activity(Long social_activity) {
 		this.social_activity = social_activity;
 	}
-
-	public String getEnrollment_object() {
+	public Long getEnrollment_object() {
 		return enrollment_object;
 	}
-
-	public void setEnrollment_object(String enrollment_object) {
+	public void setEnrollment_object(Long enrollment_object) {
 		this.enrollment_object = enrollment_object;
 	}
-
-	public String getCourse_object() {
+	public Long getCourse_object() {
 		return course_object;
 	}
-
-	public void setCourse_object(String course_object) {
+	public void setCourse_object(Long course_object) {
 		this.course_object = course_object;
 	}
-
-	public String getCourse_enrollment_object() {
+	public Long getCourse_enrollment_object() {
 		return course_enrollment_object;
 	}
-
-	public void setCourse_enrollment_object(String course_enrollment_object) {
+	public void setCourse_enrollment_object(Long course_enrollment_object) {
 		this.course_enrollment_object = course_enrollment_object;
 	}
+
 
 }
