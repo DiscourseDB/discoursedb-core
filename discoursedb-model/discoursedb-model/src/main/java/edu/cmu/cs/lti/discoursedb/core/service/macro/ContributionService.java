cc822a7e5788d0f421c85bad77a7c3d3774fd9f5
ferschke <ferschke@cs.cmu.edu>
2015-09-21 14:42:58 -0400

    Updated service javadocs

---
+++ discoursedb-model/discoursedb-model/src/main/java/edu/cmu/cs/lti/discoursedb/core/service/macro/ContributionService.java
@@ -0,0 +1,68 @@
+package edu.cmu.cs.lti.discoursedb.core.service.macro;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import edu.cmu.cs.lti.discoursedb.core.model.macro.Contribution;
+import edu.cmu.cs.lti.discoursedb.core.model.macro.ContributionType;
+import edu.cmu.cs.lti.discoursedb.core.repository.macro.ContributionRepository;
+import edu.cmu.cs.lti.discoursedb.core.repository.macro.ContributionTypeRepository;
+import edu.cmu.cs.lti.discoursedb.core.type.ContributionTypes;
+
+@Transactional(propagation= Propagation.REQUIRED, readOnly=false)
+@Service
+public class ContributionService {
+
+	@Autowired
+	private ContributionRepository contributionRepo;
+	
+	@Autowired
+	private ContributionTypeRepository contribTypeRepo;
+
+	/**
+	 * Retrieves existing or creates a new ContributionType entity with the
+	 * provided type. It then creates a new empty Contribution entity and
+	 * connects it with the type. Both changed/created entities are saved to
+	 * DiscourseDB and the empty typed Contribution is returned. It then adds
+	 * the new empty Contribution to the db and returns the object.
+	 * 
+	 * @param type
+	 *            the value for the ContributionTyep
+	 * @return a new empty Contribution that is already saved to the db and
+	 *         connected with its requested type
+	 */
+	public Contribution createTypedContribution(ContributionTypes type){		
+		Optional<ContributionType> optContribType = contribTypeRepo.findOneByType(type.name());
+		ContributionType contribType = null;
+		if(optContribType.isPresent()){
+			contribType = optContribType.get();
+		}else{
+			contribType = new ContributionType();
+			contribType.setType(type.name());
+			contribType= contribTypeRepo.save(contribType);
+		}
+
+		Contribution contrib = new Contribution();
+		contrib.setType(contribType);
+		return contributionRepo.save(contrib);
+	}		
+	
+	/**
+	 * Saves the provided entity to the db using the save method of the corresponding repository
+	 * 
+	 * @param contrib the entity to save
+	 * @return the possibly altered entity after the save process 
+	 */
+	public Contribution save(Contribution contrib){
+		return contributionRepo.save(contrib);
+	}
+	
+	public Optional<Contribution> findOneBySourceId(String id){
+		return contributionRepo.findOneBySourceId(id);		
+	}
+
+}
