a57dee000f47e6f9235f5b51707dd2f51cc6b876
Oliver Ferschke <oliver.ferschke@gmail.com>
2015-09-23 00:39:44 -0400

    All Entity beans now extend CoreBaseEntity. @Version changed to Long
    Entity beans now extend CoreBaseEntity which introduces the Version,
    last updated and created fields to each entity.
    The version field was changed from data type Date to Long, since Date
    caused problems with optimistic locking.

--- discoursedb-model/discoursedb-model/src/main/java/edu/cmu/cs/lti/discoursedb/core/model/macro/Discourse.java
+++ discoursedb-model/discoursedb-model/src/main/java/edu/cmu/cs/lti/discoursedb/core/model/macro/Discourse.java
@@ -1,7 +1,6 @@
 package edu.cmu.cs.lti.discoursedb.core.model.macro;
 
 import java.io.Serializable;
-import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -14,21 +13,20 @@ import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
-import javax.persistence.Version;
 
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 import org.hibernate.annotations.SelectBeforeUpdate;
 
+import edu.cmu.cs.lti.discoursedb.core.model.CoreBaseEntity;
 import edu.cmu.cs.lti.discoursedb.core.model.user.User;
 
 @Entity
 @SelectBeforeUpdate
 @DynamicUpdate
 @DynamicInsert
-@Table(name = "discourse", uniqueConstraints = @UniqueConstraint(columnNames = { "name", "descriptor" }) )
-public class Discourse implements Serializable {
+@Table(name = "discourse")
+public class Discourse extends CoreBaseEntity implements Serializable {
 
 	private static final long serialVersionUID = -3736157436274230022L;
 
@@ -36,34 +34,16 @@ public class Discourse implements Serializable {
 
 	private String name;
 
-	/**
-	 * Used to disambiguate the Discourse, e.g. with source information, version number or similar.
-	 * The combination of name and descriptor must be unique.
-	 */
-	private String descriptor;
-
 	private Set<DiscourseToDiscoursePart> discourseToDiscourseParts = new HashSet<DiscourseToDiscoursePart>();
 
 	private Set<User> users;
 
 	public Discourse() {}
 
-	public Discourse(String name, String descriptor){
+	public Discourse(String name){
 		this.setName(name);
-		this.setDescriptor(descriptor);
 	}
 	
-	private Date version;
-
-	@Version
-	public Date getVersion() {
-		return version;
-	}
-
-	public void setVersion(Date version) {
-		this.version = version;
-	}
-
 	@Id
 	@Column(name = "id_discourse", nullable = false)
 	@GeneratedValue(strategy = GenerationType.AUTO)
@@ -75,22 +55,13 @@ public class Discourse implements Serializable {
 		this.id = id;
 	}
 
-	@Column(updatable=false)
+	@Column(updatable=false, unique=true)
 	public String getName() {
 		return name;
 	}
 
 	public void setName(String name) {
 		this.name = name;
-	}
-
-	@Column(updatable=false)
-	public String getDescriptor() {
-		return descriptor;
-	}
-
-	public void setDescriptor(String descriptor) {
-		this.descriptor = descriptor;
 	}
 
 	@OneToMany(mappedBy = "discourse")
