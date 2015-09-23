e4d06e8f51cbc5507e1a0b2e8bb14ecfd4cb4e12
Oliver Ferschke <oliver.ferschke@gmail.com>
2015-09-23 00:40:14 -0400

    Reflecting changes in model repository

--- discoursedb-api-rest/discoursedb-api-rest/src/main/java/edu/cmu/cs/lti/discoursedb/api/core/resource/macro/DiscourseResource.java
+++ discoursedb-api-rest/discoursedb-api-rest/src/main/java/edu/cmu/cs/lti/discoursedb/api/core/resource/macro/DiscourseResource.java
@@ -1,7 +1,5 @@
 package edu.cmu.cs.lti.discoursedb.api.core.resource.macro;
 
-import java.util.Date;
-
 import org.springframework.hateoas.ResourceSupport;
 
 import edu.cmu.cs.lti.discoursedb.core.model.macro.Discourse;
@@ -10,13 +8,9 @@ public class DiscourseResource extends ResourceSupport {
 
 	private long uid;
 	private String name;
-	private String descriptor;
-	private Date lastModified;
 	
 	public DiscourseResource(Discourse discourse) {
 		this.setName(discourse.getName());
-		this.setDescriptor(discourse.getDescriptor());
-		this.setLastModified(discourse.getVersion());
 		this.setUid(discourse.getId());	
 	}
 
@@ -36,22 +30,5 @@ public class DiscourseResource extends ResourceSupport {
 	public void setName(String name) {
 		this.name = name;
 	}
-
-	public String getDescriptor() {
-		return descriptor;
-	}
-
-	public void setDescriptor(String descriptor) {
-		this.descriptor = descriptor;
-	}
-
-	public Date getLastModified() {
-		return lastModified;
-	}
-
-	public void setLastModified(Date lastModified) {
-		this.lastModified = lastModified;
-	}
-
 
 }
