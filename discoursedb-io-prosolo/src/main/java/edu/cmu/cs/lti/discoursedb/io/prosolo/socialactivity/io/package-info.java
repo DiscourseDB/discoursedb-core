6f678a9785c0ed3f1c534fe249e2aaa75f53e04b
ferschke <ferschke@cs.cmu.edu>
2015-10-16 15:08:43 -0400

    Added javadoc package descriptions

---
+++ discoursedb-io-prosolo/src/main/java/edu/cmu/cs/lti/discoursedb/io/prosolo/socialactivity/io/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Classes related to JDBC access to Prosolo using the POJOs provided in the model package.
+ * 
+ * @author Oliver Ferschke
+ */
+package edu.cmu.cs.lti.discoursedb.io.prosolo.socialactivity.io;
