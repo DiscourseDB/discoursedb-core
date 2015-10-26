5757632967e7b34971359b99e8bcebeb9ba4d369
ferschke <ferschke@cs.cmu.edu>
2015-10-26 12:25:35 -0400

    Improved javadoc

--- discoursedb-io-edx/src/main/java/edu/cmu/cs/lti/discoursedb/io/edx/forum/converter/EdxForumConverterApplication.java
+++ discoursedb-io-edx/src/main/java/edu/cmu/cs/lti/discoursedb/io/edx/forum/converter/EdxForumConverterApplication.java
@@ -3,6 +3,7 @@ package edu.cmu.cs.lti.discoursedb.io.edx.forum.converter;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.ComponentScan;
  * 
  * @author Oliver Ferschke
  */
+@SpringBootApplication
 @ComponentScan(basePackages = {"edu.cmu.cs.lti.discoursedb.configuration", "edu.cmu.cs.lti.discoursedb.io.edx.forum"})
 public class EdxForumConverterApplication {
 	
