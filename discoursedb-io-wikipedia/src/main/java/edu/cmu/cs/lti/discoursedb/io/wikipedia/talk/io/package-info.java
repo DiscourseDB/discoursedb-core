503d0213a88486d1f8f4abbc62b77cdc3d40ab0b
ferschke <ferschke@cs.cmu.edu>
2015-11-03 10:57:03 -0500

    Updated pom and package descriptions

--- discoursedb-io-wikipedia/src/main/java/edu/cmu/cs/lti/discoursedb/io/wikipedia/talk/io/package-info.java
+++ discoursedb-io-wikipedia/src/main/java/edu/cmu/cs/lti/discoursedb/io/wikipedia/talk/io/package-info.java
@@ -1,5 +1,6 @@
 /**
  * This package contains the data providers for the Talk page converters.
+ * IO classes can either access pre-extracted data, raw data dumps or JWPL databases. 
  * 
  * @author Oliver Ferschke
  */
---
+++ discoursedb-io-wikipedia/src/main/java/edu/cmu/cs/lti/discoursedb/io/wikipedia/talk/model/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * POJOs that represent Wikipedia Talk data independent from the io module used to retrieve it.
+ * 
+ * @author Oliver Ferschke
+ */
+package edu.cmu.cs.lti.discoursedb.io.wikipedia.talk.model;
