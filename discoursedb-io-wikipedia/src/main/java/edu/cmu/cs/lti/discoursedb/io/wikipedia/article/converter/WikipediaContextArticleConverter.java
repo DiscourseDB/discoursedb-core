1aaaff33ee4d1c8ece0bd0384ab684cb424249dc
ferschke <ferschke@cs.cmu.edu>
2015-12-04 17:14:09 -0500

    Changes in context converter - still buggy

--- discoursedb-io-wikipedia/src/main/java/edu/cmu/cs/lti/discoursedb/io/wikipedia/article/converter/WikipediaContextArticleConverter.java
+++ discoursedb-io-wikipedia/src/main/java/edu/cmu/cs/lti/discoursedb/io/wikipedia/article/converter/WikipediaContextArticleConverter.java
@@ -1,7 +1,9 @@
 package edu.cmu.cs.lti.discoursedb.io.wikipedia.article.converter;
 
 import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -14,11 +16,18 @@ import de.tudarmstadt.ukp.wikipedia.api.Page;
 import de.tudarmstadt.ukp.wikipedia.api.WikiConstants.Language;
 import de.tudarmstadt.ukp.wikipedia.api.Wikipedia;
 import de.tudarmstadt.ukp.wikipedia.api.hibernate.WikiHibernateUtil;
+import de.tudarmstadt.ukp.wikipedia.revisionmachine.api.Revision;
 import de.tudarmstadt.ukp.wikipedia.revisionmachine.api.RevisionApi;
 import de.tudarmstadt.ukp.wikipedia.revisionmachine.api.RevisionIterator;
+import edu.cmu.cs.lti.discoursedb.core.model.macro.Context;
+import edu.cmu.cs.lti.discoursedb.core.model.macro.Discourse;
 import edu.cmu.cs.lti.discoursedb.core.model.macro.DiscoursePart;
+import edu.cmu.cs.lti.discoursedb.core.service.macro.ContentService;
+import edu.cmu.cs.lti.discoursedb.core.service.macro.ContextService;
 import edu.cmu.cs.lti.discoursedb.core.service.macro.DiscoursePartService;
+import edu.cmu.cs.lti.discoursedb.core.service.macro.DiscourseService;
 import edu.cmu.cs.lti.discoursedb.core.type.DiscoursePartTypes;
+import edu.cmu.cs.lti.discoursedb.io.wikipedia.article.model.ContextTransactionData;
 
 /**
  * 
@@ -28,17 +37,25 @@ import edu.cmu.cs.lti.discoursedb.core.type.DiscoursePartTypes;
 @Component
 public class WikipediaContextArticleConverter implements CommandLineRunner {
 
-	private static final Logger logger = LogManager.getLogger(WikipediaContextArticleConverter.class);	
+	private static final Logger logger = LogManager.getLogger(WikipediaContextArticleConverter.class);
 
-	@Autowired private DiscoursePartService discoursePartService;
-	@Autowired private WikipediaContextArticleConverterService converterService;
+	@Autowired
+	private DiscoursePartService discoursePartService;
+	@Autowired
+	private WikipediaContextArticleConverterService converterService;
+	@Autowired
+	private ContextService contextService;
+	@Autowired
+	private DiscourseService discourseService;
+	@Autowired
+	private ContentService contentService;
 
 	@Override
 	public void run(String... args) throws Exception {
-		if(args.length!=5){			
+		if (args.length != 5) {
 			throw new RuntimeException("Incorrect number of launch parameters.");
 		}
-		
+
 		logger.trace("Establishing connection to Wikipedia db...");
 		DatabaseConfiguration dbconf = new DatabaseConfiguration();
 		dbconf.setHost(args[0]);
@@ -48,37 +65,65 @@ public class WikipediaContextArticleConverter implements CommandLineRunner {
 		dbconf.setLanguage(Language.valueOf(args[4]));
 		Wikipedia wiki = new Wikipedia(dbconf);
 		RevisionApi revApi = new RevisionApi(dbconf);
-		
+
 		List<DiscoursePart> talkPageDPs = discoursePartService.findAllByType(DiscoursePartTypes.TALK_PAGE);
-		logger.info("Start mapping context articles for "+talkPageDPs.size()+" existing Talk pages");		
+		logger.info("Start mapping context articles for " + talkPageDPs.size() + " existing Talk pages");
 
-		for(DiscoursePart curTalkPageDP:talkPageDPs){
+		for (DiscoursePart curTalkPageDP : talkPageDPs) {
+			ContextTransactionData contextTransactionData =  converterService.mapContext(curTalkPageDP);
+
+			//retrieve the discourse for the provided TalkPage
+			Optional<Discourse> discourse = discourseService.findOne(curTalkPageDP);
+			if(!discourse.isPresent()){
+				logger.error("Could not retrieve the Discourse for the provided Talk Page DiscoursePart");
+				continue;
+			}
 			
-			//determine the timestamp of the first and last contribution of this Talk page
-			//this determines the window of article revisions we are going to retrieve
-			Timestamp timeOfFirstContrib = null; //TODO  get contribution timestamp
-			Timestamp timeOfLastContrib = null; //TODO get contribution timestamp
-
-			//get reference to the article for the given Talk page
+			// get reference to the article for the given Talk page
 			Page article = wiki.getPage(curTalkPageDP.getName());
 			int articleId = article.getPageId();
-			
-			//get primary keys for first and last revision in the window
-			List<Timestamp> revTimestamps = revApi.getRevisionTimestampsBetweenTimestamps(article.getPageId(), timeOfFirstContrib, timeOfLastContrib);			
-			int firstRevPK = revApi.getRevision(articleId, revTimestamps.get(0)).getPrimaryKey();
-			int lastRevPK =  revApi.getRevision(articleId, revTimestamps.get(revTimestamps.size()-1)).getPrimaryKey();
 
-			//create revision iterator that iterates over the article revisions between the two provided contribution timestamps
-			RevisionIterator articleRevIt = new RevisionIterator(revApi.getRevisionApiConfiguration(),firstRevPK,lastRevPK, revApi.getConnection());
+			// get primary keys for first and last revision in the window
+			List<Timestamp> revTimestamps = revApi.getRevisionTimestampsBetweenTimestamps(articleId, new Timestamp(contextTransactionData.getFirstContent().getTime()), new Timestamp(contextTransactionData.getLastContent().getTime()));
+			int firstRevCounter = revApi.getRevision(articleId, revTimestamps.get(0)).getRevisionCounter();
+			int lastRevPK = revApi.getRevision(articleId, revTimestamps.get(revTimestamps.size() - 1)).getPrimaryKey();
+
+			// create revision iterator that iterates over the article revisions
+			// between the two provided contribution timestamps
+			RevisionIterator articleRevIt = new RevisionIterator(revApi.getRevisionApiConfiguration(), revApi.getFirstRevisionPK(articleId), lastRevPK, revApi.getConnection());
+			articleRevIt.setShouldLoadRevisionText(false);
+
+			//Process revisions and create content objects.
+			//The content objects will represent a doubly linked list and they are eventually associated with the same context
+			List<Long> ids = new ArrayList<>(); //keeps track of the previous and first revision 
+			while(articleRevIt.hasNext()){
+				Revision curArticleRev = articleRevIt.next();
+				if(curArticleRev.getRevisionCounter()<firstRevCounter){
+					// the RevisionIterator can only begin at the first revision and
+					// not an in-between revision, so we have to skip the revisions
+					// until the revisioncounter fits the beginning of our target window
+					// since we set the iterator to not reconstruct revisions automatically,
+					// this shouldn't be expensive
+					continue;
+				}
+				ids.add(converterService.mapRevision(discourse.get().getId(),curArticleRev,article.getTitle().getPlainTitle()));
+			}		
 			
-			//start mapping the article revisions 
-			converterService.mapContextArticleRevisions(curTalkPageDP, article, articleRevIt);
+//TODO		linking the content entities causes problems
+			converterService.linkRevisions(ids);
+			
+			//update reference to first and last content element 
+			//start and end time are already created
+			Context ctx = contextService.findOne(contextTransactionData.getContextId());
+			ctx.setFirstRevision(contentService.findOne(ids.get(0)));
+			ctx.setCurrentRevision(contentService.findOne(ids.get(ids.size()-1)));
+			
 		}
-		
+
 		logger.info("Finished mapping context articles.");
 
-		//manually close the hibernate session for the Wikipedia connection which is not managed by Spring
+		// manually close the hibernate session for the Wikipedia connection
+		// which is not managed by Spring
 		WikiHibernateUtil.getSessionFactory(dbconf).close();
 	}
-
 }
--- discoursedb-io-wikipedia/src/main/java/edu/cmu/cs/lti/discoursedb/io/wikipedia/article/converter/WikipediaContextArticleConverterService.java
+++ discoursedb-io-wikipedia/src/main/java/edu/cmu/cs/lti/discoursedb/io/wikipedia/article/converter/WikipediaContextArticleConverterService.java
@@ -1,22 +1,33 @@
 package edu.cmu.cs.lti.discoursedb.io.wikipedia.article.converter;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import de.tudarmstadt.ukp.wikipedia.api.Page;
 import de.tudarmstadt.ukp.wikipedia.api.exception.WikiApiException;
-import de.tudarmstadt.ukp.wikipedia.revisionmachine.api.RevisionIterator;
+import de.tudarmstadt.ukp.wikipedia.revisionmachine.api.Revision;
+import edu.cmu.cs.lti.discoursedb.core.model.macro.Content;
+import edu.cmu.cs.lti.discoursedb.core.model.macro.Context;
+import edu.cmu.cs.lti.discoursedb.core.model.macro.Contribution;
 import edu.cmu.cs.lti.discoursedb.core.model.macro.DiscoursePart;
+import edu.cmu.cs.lti.discoursedb.core.model.user.User;
 import edu.cmu.cs.lti.discoursedb.core.service.macro.ContentService;
+import edu.cmu.cs.lti.discoursedb.core.service.macro.ContextService;
 import edu.cmu.cs.lti.discoursedb.core.service.macro.ContributionService;
 import edu.cmu.cs.lti.discoursedb.core.service.macro.DiscoursePartService;
 import edu.cmu.cs.lti.discoursedb.core.service.macro.DiscourseService;
-import edu.cmu.cs.lti.discoursedb.core.service.system.DataSourceService;
 import edu.cmu.cs.lti.discoursedb.core.service.user.UserService;
+import edu.cmu.cs.lti.discoursedb.core.type.ContextTypes;
+import edu.cmu.cs.lti.discoursedb.core.type.DiscoursePartRelationTypes;
+import edu.cmu.cs.lti.discoursedb.io.wikipedia.article.model.ContextTransactionData;
 
 /**
  * This service maps pre-segmented TalkPage objects to DiscourseDB entities 
@@ -27,23 +38,102 @@ import edu.cmu.cs.lti.discoursedb.core.service.user.UserService;
 @Service
 public class WikipediaContextArticleConverterService{
 
-	private static final Logger logger = LogManager.getLogger(WikipediaContextArticleConverterService.class);
-	
+	@PersistenceContext private EntityManager entityManager;
+	@Autowired private ContentService contentService;
 	@Autowired private DiscourseService discourseService;
-	@Autowired private DataSourceService dataSourceService;
 	@Autowired private DiscoursePartService discoursePartService;
 	@Autowired private ContributionService contributionService;
-	@Autowired private ContentService contentService;
 	@Autowired private UserService userService;
+	@Autowired private ContextService contextService;
+	
+	public ContextTransactionData mapContext(DiscoursePart curTalkPageDP) throws WikiApiException{
+		/*
+		 * Load Contributions for the given Talk page and determine the time of
+		 * the first and the last contribution.
+		 */
+		List<Contribution> tpContribs = new ArrayList<>();
+		Date timeOfFirstContrib = null;
+		Date timeOfLastContrib = null;
+		//we need references to all contributions anyway, so we can determine the time of first/last contrib in TP 
+		//while we load the contributions rather than making an extra query
+		for(DiscoursePart curDiscussionDP:discoursePartService.findChildDiscourseParts(curTalkPageDP, DiscoursePartRelationTypes.TALK_PAGE_HAS_DISCUSSION)){
+			for(Contribution contrib:contributionService.findAllByDiscoursePart(curDiscussionDP)){
+				if(timeOfFirstContrib==null&&timeOfLastContrib==null){
+					timeOfFirstContrib=contrib.getStartTime();
+					timeOfLastContrib=contrib.getStartTime();
+				}else{
+					if(contrib.getStartTime().before(timeOfFirstContrib)){
+						timeOfFirstContrib=contrib.getStartTime();
+					}
+					if(contrib.getStartTime().after(timeOfLastContrib)){
+						timeOfLastContrib=contrib.getStartTime();						
+					}					
+				}
+				tpContribs.add(contrib);
+			}
+		}
+
+		//create context entity for the previously created content entities
+		Context curTPContext = contextService.createTypedContext(ContextTypes.ARTICLE);
+		curTPContext.setStartTime(timeOfFirstContrib);
+		curTPContext.setEndTime(timeOfLastContrib);		
+		
+		//connect context with all contributions of the corresponding Talk page
+		for(Contribution contrib:tpContribs){
+			contextService.addContributionToContext(curTPContext, contrib);
+		}
+		return new ContextTransactionData(timeOfFirstContrib, timeOfLastContrib, curTPContext.getId());
+	}
+
 	
 	/**
-	 * Maps the provided article revisions to a context and corresponding content entities and associates them with the contributions.
+	 * Maps a single revision to a content entity in DiscourseDB.
 	 * 
-	 * @param talkPageDP the DiscoursePart that represents the Talk page
-	 * @param article the article that constitutes the context of the provided Talk page
-	 * @param articleRevs the revisions of the article that are relevant for the context of this Talk page
+	 * @param discourse the discourse the content is part of (for user generation) 
+	 * @param curArticleRev the revision to map
+	 * @param articleTitle the title of the article the talk page belongs to
 	 */
-	public void mapContextArticleRevisions(DiscoursePart talkPageDP, Page article, RevisionIterator revIt) throws WikiApiException{
+	public Long mapRevision(Long discourseId, Revision curArticleRev, String articleTitle){
+		User curUser = userService.createOrGetUser(discourseService.findOne(discourseId), curArticleRev.getContributorName());			
+				
+		Content curRev = contentService.createContent();
+		curRev.setText(curArticleRev.getRevisionText());
+		curRev.setAuthor(curUser);
+		curRev.setStartTime(curArticleRev.getTimeStamp());
+		curRev.setTitle(articleTitle);
 		
+		return curRev.getId();
 	}
+	
+	/**
+	 * Links all content 
+	 */
+	public void linkRevisions(List<Long> ids){
+		Content previous = null;
+		for(Content current:contentService.findAll(ids)){  //TODO make sure the order of Content objects is correct
+			if(previous!=null){
+				previous.setNextRevision(current);
+				current.setPreviousRevision(previous);
+				previous.setEndTime(current.getStartTime());				
+   		        entityManager.flush(); //FIXME There must be a better way than flushing for every update. Doesn't work for long lists
+			    entityManager.clear();
+			}
+			previous=current;
+		}
+	}
+	
+	/**
+	 * Update references to first and last element 
+	 */
+	public Context updateContext(Long contextId, Long firstContentId, Long lastContentId){
+		Context ctx = contextService.findOne(contextId);
+		Content first = contentService.findOne(firstContentId);
+		Content last = contentService.findOne(lastContentId);
+		ctx.setFirstRevision(first);
+		ctx.setCurrentRevision(last);
+		ctx.setStartTime(first.getStartTime());
+		ctx.setEndTime(last.getEndTime());
+		return contextService.save(ctx);
+	}
+
 }
