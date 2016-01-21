1cb64bfda8fa70b5a12e228be2b38d6edb209865
ferschke <ferschke@cs.cmu.edu>
2016-01-21 11:19:51 -0500

    Basic UI and NPM setup

---
+++ discoursedb-annotation-ui/src/main/java/edu/cmu/cs/lti/discoursedb/annotation/ui/controller/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Contains the MVC controllers for the UI
+ * 
+ * @author Oliver Ferschke
+ *
+ */
+package edu.cmu.cs.lti.discoursedb.annotation.ui.controller;
---
+++ discoursedb-annotation-ui/src/main/java/edu/cmu/cs/lti/discoursedb/annotation/ui/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Contains the backend for the DiscourseDB Annotation Web UI
+ * 
+ * @author Oliver Ferschke
+ *
+ */
+package edu.cmu.cs.lti.discoursedb.annotation.ui;
