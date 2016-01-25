42fc8fa51cea32cd5a7d25e4994a5ade045bc43e
ferschke <ferschke@cs.cmu.edu>
2016-01-25 15:21:28 -0500

    Lombok'ed cousera model

--- discoursedb-io-coursera/src/main/java/edu/cmu/cs/lti/discoursedb/io/coursera/model/Comment.java
+++ discoursedb-io-coursera/src/main/java/edu/cmu/cs/lti/discoursedb/io/coursera/model/Comment.java
@@ -1,14 +1,19 @@
 package edu.cmu.cs.lti.discoursedb.io.coursera.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 /**
  * Wraps entities form the forum_comments table in coursera database
  * 
  * @author Haitian Gong
+ * @author Oliver Ferschke
  *
  */
-
+@Data
+@AllArgsConstructor
 public class Comment {
-	
+
 	private long id;
 	private long thread_id;
 	private long post_id;
@@ -17,83 +22,5 @@ public class Comment {
 	private String text;
 	private int delete;
 	private long post_time;
-	
-	public Comment(long id, long thread_id, long post_id, 
-			long user_id, long votes, String text, int delete, long post_time) {
-		this.id = id;
-		this.thread_id = thread_id;
-		this.post_id = post_id;
-		this.user_id = user_id;
-		this.votes = votes;
-		this.text = text;
-		this.delete = delete;
-		this.setPost_time(post_time);
-	}
-
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	public long getThread_id() {
-		return thread_id;
-	}
-
-	public void setThread_id(long thread_id) {
-		this.thread_id = thread_id;
-	}
-
-	public long getPost_id() {
-		return post_id;
-	}
-
-	public void setPost_id(long post_id) {
-		this.post_id = post_id;
-	}
-
-	public long getUser_id() {
-		return user_id;
-	}
-
-	public void setUser_id(long user_id) {
-		this.user_id = user_id;
-	}
-
-	public long getVotes() {
-		return votes;
-	}
-
-	public void setVotes(long votes) {
-		this.votes = votes;
-	}
-
-	public String getText() {
-		return text;
-	}
-
-	public void setText(String text) {
-		this.text = text;
-	}
-
-	public int getDelete() {
-		return delete;
-	}
-
-	public void setDelete(int delete) {
-		this.delete = delete;
-	}
-
-	public long getPost_time() {
-		return post_time;
-	}
-
-	public void setPost_time(long post_time) {
-		this.post_time = post_time;
-	}
-	
-	
 	
 }
--- discoursedb-io-coursera/src/main/java/edu/cmu/cs/lti/discoursedb/io/coursera/model/Forum.java
+++ discoursedb-io-coursera/src/main/java/edu/cmu/cs/lti/discoursedb/io/coursera/model/Forum.java
@@ -1,58 +1,21 @@
 package edu.cmu.cs.lti.discoursedb.io.coursera.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 /**
  * Wraps entities form the forum_forums table in coursera database
  * 
  * @author Haitian Gong
- *
+ * @author Oliver Ferschke
  */
-
+@Data
+@AllArgsConstructor
 public class Forum {
 	
 	private long id;
 	private long parent_id;
 	private String name;
 	private long opentime;
-	
-	public Forum(long id, long parent_id, String name, long opentime) {
-		this.setId(id);
-		this.setParent_id(parent_id);
-		this.setName(name);
-		this.setOpentime(opentime);
-	}
-
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	public long getParent_id() {
-		return parent_id;
-	}
-
-	public void setParent_id(long parent_id) {
-		this.parent_id = parent_id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public long getOpentime() {
-		return opentime;
-	}
-
-	public void setOpentime(long opentime) {
-		this.opentime = opentime;
-	}
-	
-	
 	
 }
