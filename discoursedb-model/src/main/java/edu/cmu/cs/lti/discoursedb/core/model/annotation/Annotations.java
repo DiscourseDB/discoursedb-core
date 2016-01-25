df391c1871d49108fc5f8363dd86b4759a333ddd
ferschke <ferschke@cs.cmu.edu>
2016-01-25 17:32:17 -0500

    Fixed circular dependencies in hashCode() implementations

--- discoursedb-model/src/main/java/edu/cmu/cs/lti/discoursedb/core/model/annotation/Annotations.java
+++ discoursedb-model/src/main/java/edu/cmu/cs/lti/discoursedb/core/model/annotation/Annotations.java
@@ -14,38 +14,35 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-import edu.cmu.cs.lti.discoursedb.core.model.UntimedBaseEntity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import edu.cmu.cs.lti.discoursedb.core.model.UntimedBaseEntity;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Setter;
+import lombok.ToString;
+
+@Data
+@EqualsAndHashCode(callSuper=true, exclude={"annotations"})
+@ToString(callSuper=true, exclude={"annotations"})
 @Entity
 @Table(name="annotation")
 public class Annotations extends UntimedBaseEntity implements Serializable{
 
 	private static final long serialVersionUID = -4654984158138436217L;
 
-	private long id;
-	
-    private Set<AnnotationInstance> annotations = new HashSet<AnnotationInstance>();
-    
-	public Annotations(){}
-	
 	@Id
 	@Column(name="id_annotation", nullable=false)
     @GeneratedValue(strategy = GenerationType.AUTO)
-	public long getId() {
-		return id;
-	}
-
-	@SuppressWarnings("unused") //used by hibernate through reflection, but not exposed to users
-	private void setId(long id) {
-		this.id = id;
-	}
+	@Setter(AccessLevel.PRIVATE) 
+	private Long id;
+	
 	@OneToMany(fetch=FetchType.LAZY,cascade=CascadeType.ALL,mappedBy="annotationAggregate")
-	public Set<AnnotationInstance> getAnnotations() {
-		return annotations;
+	@JsonIgnore
+	private Set<AnnotationInstance> annotations = new HashSet<AnnotationInstance>();
+    
+	public void addAnnotation(AnnotationInstance annotation) {
+		this.annotations.add(annotation);
 	}
-
-	public void setAnnotations(Set<AnnotationInstance> annotations) {
-		this.annotations = annotations;
-	}
-
 }
