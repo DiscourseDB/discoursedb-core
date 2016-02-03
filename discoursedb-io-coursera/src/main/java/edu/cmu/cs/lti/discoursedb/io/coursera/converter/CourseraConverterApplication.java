d4b7098c8d3d8a0b658c2f4373f8a4baa642168f
ferschke <ferschke@cs.cmu.edu>
2016-02-03 16:29:16 -0500

    Added check of command line params

--- discoursedb-io-coursera/src/main/java/edu/cmu/cs/lti/discoursedb/io/coursera/converter/CourseraConverterApplication.java
+++ discoursedb-io-coursera/src/main/java/edu/cmu/cs/lti/discoursedb/io/coursera/converter/CourseraConverterApplication.java
@@ -3,6 +3,7 @@ package edu.cmu.cs.lti.discoursedb.io.coursera.converter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.util.Assert;
 
 import edu.cmu.cs.lti.discoursedb.io.coursera.converter.CourseraConverterApplication;
 
@@ -13,6 +14,7 @@ import edu.cmu.cs.lti.discoursedb.io.coursera.converter.CourseraConverterApplica
  * This starter class requires six parameters: DataSetName, DiscourseName, coursera_dbhost, coursera_db, coursera_dbuser, coursera_dbpwd
  * 
  * @author Haitian Gong
+ * @author Oliver Ferschke
  */
 @SpringBootApplication
 @ComponentScan(basePackages={"edu.cmu.cs.lti.discoursedb.configuration","edu.cmu.cs.lti.discoursedb.io.coursera"})
@@ -23,7 +25,7 @@ public class CourseraConverterApplication {
 	 *         DataSetName, DiscourseName, coursera_dbhost, coursera_db, coursera_dbuser, coursera_dbpwd
 	 */
 	public static void main(String[] args) {
-		
+		Assert.isTrue(args.length==6,"Usage: CourseraConverterApplication <DataSetName> <DiscourseName> <coursera_dbhost> <coursera_db> <coursera_dbuser> <coursera_dbpwd>");
 		SpringApplication.run(CourseraConverterApplication.class, args);
 		
 	}
