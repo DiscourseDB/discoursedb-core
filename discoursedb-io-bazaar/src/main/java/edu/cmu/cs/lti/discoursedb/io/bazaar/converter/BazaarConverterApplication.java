a2d9ea272d8bcf4ebdcd00bba646ebc084dcfe49
ferschke <ferschke@cs.cmu.edu>
2016-04-04 12:29:48 -0400

    Robustness and code quality

--- discoursedb-io-bazaar/src/main/java/edu/cmu/cs/lti/discoursedb/io/bazaar/converter/BazaarConverterApplication.java
+++ discoursedb-io-bazaar/src/main/java/edu/cmu/cs/lti/discoursedb/io/bazaar/converter/BazaarConverterApplication.java
@@ -5,6 +5,7 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.util.Assert;
 
 /**
  * 
@@ -16,14 +17,13 @@ import org.springframework.context.annotation.ComponentScan;
  * 
  *  
  * @author Haitian Gong
+ * @author Oliver Ferschke
  *
  */
 
 @SpringBootApplication
 @ComponentScan(basePackages={"edu.cmu.cs.lti.discoursedb.configuration","edu.cmu.cs.lti.discoursedb.io.bazaar"})
 public class BazaarConverterApplication {
-
-	private static final Logger logger = LogManager.getLogger(BazaarConverterApplication.class);
 	
 	/**
 	 * @param args 
@@ -34,10 +34,7 @@ public class BazaarConverterApplication {
 	 */
 	
 	public static void main(String[] args) {		
-		if (args.length != 4) {
-			logger.error("Usage: BazaarConverterApplication <Dataset Name> <Discourse name> <chat message log> <chat room log>");
-			return;
-		}
+		Assert.isTrue(args.length==4,"Usage: BazaarConverterApplication <Dataset Name> <Discourse name> <chat message log> <chat room log>");
 		SpringApplication.run(BazaarConverterApplication.class, args);		
 	}
 
