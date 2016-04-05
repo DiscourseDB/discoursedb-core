d08b3b673fcf7a73ed33370e543b561dafd5a5ff
ferschke <ferschke@cs.cmu.edu>
2016-04-05 11:21:03 -0400

    Removed unused import

--- discoursedb-io-bazaar/src/main/java/edu/cmu/cs/lti/discoursedb/io/bazaar/converter/BazaarConverterApplication.java
+++ discoursedb-io-bazaar/src/main/java/edu/cmu/cs/lti/discoursedb/io/bazaar/converter/BazaarConverterApplication.java
@@ -1,7 +1,5 @@
 package edu.cmu.cs.lti.discoursedb.io.bazaar.converter;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
