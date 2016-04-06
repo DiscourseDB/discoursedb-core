3db64b7b577337164a92af57ad47d0f7979a95c2
ferschke <ferschke@cs.cmu.edu>
2016-04-06 14:09:12 -0400

    General improvements in Habworlds code

--- discoursedb-io-habworlds/src/main/java/edu/cmu/cs/lti/discoursedb/io/habworlds/converter/HabworldsConverterApplication.java
+++ discoursedb-io-habworlds/src/main/java/edu/cmu/cs/lti/discoursedb/io/habworlds/converter/HabworldsConverterApplication.java
@@ -3,6 +3,7 @@ package edu.cmu.cs.lti.discoursedb.io.habworlds.converter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.util.Assert;
 
 
 @SpringBootApplication
@@ -17,6 +18,7 @@ public class HabworldsConverterApplication {
 	 */
 
 	public static void main(String[] args) {
+		Assert.isTrue(args.length==3, "Usage: HabworldsConverterApplication <DiscourseName> <DataSetName> <HabworldsFilePath>");
 		SpringApplication.run(HabworldsConverterApplication.class, args);
 	}
 
--- discoursedb-io-habworlds/src/main/java/edu/cmu/cs/lti/discoursedb/io/habworlds/converter/HabworldsConverterService.java
+++ discoursedb-io-habworlds/src/main/java/edu/cmu/cs/lti/discoursedb/io/habworlds/converter/HabworldsConverterService.java
@@ -13,7 +13,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
-import org.w3c.dom.*;
+import org.w3c.dom.CharacterData;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 
 import edu.cmu.cs.lti.discoursedb.core.model.macro.Content;
@@ -35,141 +39,137 @@ import edu.cmu.cs.lti.discoursedb.io.habworlds.model.HabWorldPost;
 import edu.cmu.cs.lti.discoursedb.io.habworlds.model.HabworldsSourceMapping;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j;
 
-
+@Log4j
 @Service
-@Transactional(propagation=Propagation.REQUIRED, readOnly=false)
-@RequiredArgsConstructor(onConstructor = @__(@Autowired) )
+@Transactional(propagation = Propagation.REQUIRED, readOnly = false)
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class HabworldsConverterService {
-	
+
 	private final @NonNull DataSourceService dataSourceService;
 	private final @NonNull UserService userService;
 	private final @NonNull ContentService contentService;
 	private final @NonNull ContributionService contributionService;
 	private final @NonNull DiscoursePartService discoursepartService;
 	private final @NonNull DiscourseService discourseService;
-	
+
+	private static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
 	public void mapPost(HabWorldPost post, String discourseName, String datasetName) throws ParseException {
-		
-		Discourse discourse = discourseService.createOrGetDiscourse(discourseName);
-		
-		/* 
-		 * check whether the question already exists in DiscourseDB
-		 * if not, create a new DiscoursePart object and add it to data source
-		 * 
-		 */
-		
-		Optional<DiscoursePart> existingQuestion = discoursepartService.findOneByDataSource(
-				post.getQuestionID(), HabworldsSourceMapping.ID_STR_TO_DISCOURSEPART, datasetName);
-		
-		if(!existingQuestion.isPresent()) {
-			DiscoursePart question = discoursepartService.createTypedDiscoursePart(discourse, DiscoursePartTypes.CHATROOM);
-			dataSourceService.addSource(question, new DataSourceInstance(
-					String.valueOf(post.getQuestionID()), 
-					HabworldsSourceMapping.ID_STR_TO_DISCOURSEPART, 
-					DataSourceTypes.HABWORLDS, 
-					datasetName));
+
+		//parse XML and skip post if XML is malformed
+		String postText = null;
+		try{
+			postText = parseXml(post.getSnapshot());
+		}catch(Exception e){
+			log.error("Unable to parse XML of post: "+post.getSnapshot());
+			return;
 		}
-		
-		/* 
-		 * check whether the post contribution all exists in DiscourseDB
-		 * if not, create a new Contribution object and add it to data source
-		 * and also create related User and Content object
+
+		Discourse discourse = discourseService.createOrGetDiscourse(discourseName);
+
+		/*
+		 * check whether the question already exists in DiscourseDB if not,
+		 * create a new DiscoursePart object and add it to data source
 		 * 
 		 */
-		
-		Optional<Contribution> existingPost = contributionService.findOneByDataSource(
-				String.valueOf(post.getInteractionID()), HabworldsSourceMapping.ID_STR_TO_CONTRIBUTION, datasetName);
-		
-		if(!existingPost.isPresent()) {
-			Contribution curPost = contributionService.createTypedContribution(ContributionTypes.POST);
-			
+
+		discoursepartService.findOneByDataSource(post.getQuestionID(), HabworldsSourceMapping.ID_STR_TO_DISCOURSEPART, datasetName)
+				.orElseGet(() -> {
+					DiscoursePart question = discoursepartService.createTypedDiscoursePart(discourse,
+							DiscoursePartTypes.CHATROOM);
+					dataSourceService.addSource(question, new DataSourceInstance(post.getQuestionID(),
+							HabworldsSourceMapping.ID_STR_TO_DISCOURSEPART, DataSourceTypes.HABWORLDS, datasetName));
+					return question;
+				}
+		);
+
+		/*
+		 * check whether the post contribution all exists in DiscourseDB if not,
+		 * create a new Contribution object and add it to data source and also
+		 * create related User and Content object
+		 * 
+		 */
+
+		Optional<Contribution> existingPost = contributionService.findOneByDataSource(String.valueOf(post.getInteractionID()), HabworldsSourceMapping.ID_STR_TO_CONTRIBUTION, datasetName);
+
+		if (!existingPost.isPresent()) {
+			Contribution curContribution = contributionService.createTypedContribution(ContributionTypes.POST);
+
 			// set start and end time
-			
-			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
 			Date date = sdf.parse(post.getServerTime());
-			curPost.setStartTime(date);
-			curPost.setEndTime(date);
-			
+			curContribution.setStartTime(date);
+			curContribution.setEndTime(date);
+
 			// set content
-			
+
 			User author = userService.createOrGetUser(discourse, String.valueOf(post.getUserID()));
-			dataSourceService.addSource(author,
-					new DataSourceInstance(
-							String.valueOf(post.getUserID()), 
-							HabworldsSourceMapping.FROM_USER_ID_STR_TO_USER, 
-							DataSourceTypes.HABWORLDS, 
-							datasetName));
+			dataSourceService.addSource(author, new DataSourceInstance(String.valueOf(post.getUserID()),
+					HabworldsSourceMapping.FROM_USER_ID_STR_TO_USER, DataSourceTypes.HABWORLDS, datasetName));
 
 			Content curContent = contentService.createContent();
-			curContent.setText(this.parseXml(post.getSnapshot()));
+			curContent.setText(postText);
 			curContent.setStartTime(date);
 			curContent.setEndTime(date);
 			curContent.setAuthor(author);
-			dataSourceService.addSource(curContent,
-					new DataSourceInstance(
-							String.valueOf(post.getInteractionID()), 
-							HabworldsSourceMapping.ID_STR_TO_CONTENT, 
-							DataSourceTypes.HABWORLDS, 
-							datasetName));
-			
-			curPost.setFirstRevision(curContent);
-			curPost.setCurrentRevision(curContent);
-			dataSourceService.addSource(curPost,
-					new DataSourceInstance(
-							String.valueOf(post.getInteractionID()), 
-							HabworldsSourceMapping.ID_STR_TO_CONTRIBUTION, 
-							DataSourceTypes.HABWORLDS, 
-							datasetName));
-			
+			dataSourceService.addSource(curContent, new DataSourceInstance(String.valueOf(post.getInteractionID()),
+					HabworldsSourceMapping.ID_STR_TO_CONTENT, DataSourceTypes.HABWORLDS, datasetName));
+
+			curContribution.setFirstRevision(curContent);
+			curContribution.setCurrentRevision(curContent);
+			dataSourceService.addSource(curContribution, new DataSourceInstance(String.valueOf(post.getInteractionID()),
+					HabworldsSourceMapping.ID_STR_TO_CONTRIBUTION, DataSourceTypes.HABWORLDS, datasetName));
+
 			// add the contribution to the question it belongs to
-			
-			DiscoursePart question = 
-					discoursepartService.createOrGetTypedDiscoursePart(
-							discourse, post.getQuestionID(), DiscoursePartTypes.CHATROOM);
-			
-			discoursepartService.addContributionToDiscoursePart(curPost, question);
-			
+
+			DiscoursePart question = discoursepartService.createOrGetTypedDiscoursePart(discourse, post.getQuestionID(),
+					DiscoursePartTypes.CHATROOM);
+
+			discoursepartService.addContributionToDiscoursePart(curContribution, question);
 		}
-		
+
 	}
-	
-	private String parseXml(String xml) {
+
+	/**
+	 * TODO: create test case for this
+	 * 
+	 * @param xml
+	 *            xml String
+	 * @return the text extracted from the xml String
+	 */
+	private String parseXml(String xml) throws Exception{
 		String text = null;
-		
-		try {
-	        DocumentBuilderFactory dbf =
-	            DocumentBuilderFactory.newInstance();
-	        DocumentBuilder db = dbf.newDocumentBuilder();
-	        InputSource is = new InputSource();
-	        is.setCharacterStream(new StringReader(xml));
 
-	        Document doc = db.parse(is);
-	        NodeList nodes = doc.getElementsByTagName("entry");
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		DocumentBuilder db = dbf.newDocumentBuilder();
+		InputSource is = new InputSource();
+		is.setCharacterStream(new StringReader(xml));
 
-	        for(int i=0;i<nodes.getLength();i++) {
-	        	Element element = (Element) nodes.item(i);
-		        NodeList name = element.getElementsByTagName("string");
-		        if(name.getLength()==2) {
-		        	Element line = (Element) name.item(1);
-			        text = new String(getCharacterDataFromElement(line));
-			        break;
-		        }
-	        }
-	    }
-	    catch (Exception e) {
-	        e.printStackTrace();
-	    }
+		Document doc = db.parse(is);
+		NodeList nodes = doc.getElementsByTagName("entry");
+
+		for (int i = 0; i < nodes.getLength(); i++) {
+			Element element = (Element) nodes.item(i);
+			NodeList name = element.getElementsByTagName("string");
+			if (name.getLength() == 2) {
+				Element line = (Element) name.item(1);
+				text = new String(getCharacterDataFromElement(line));
+				break;
+			}
+		}
 		return text;
+
 	}
-	
+
 	private String getCharacterDataFromElement(Element e) {
-	    Node child = e.getFirstChild();
-	    if (child instanceof CharacterData) {
-	       CharacterData cd = (CharacterData) child;
-	       return cd.getData();
-	    }
-	    return "?";
-	  }
+		Node child = e.getFirstChild();
+		if (child instanceof CharacterData) {
+			CharacterData cd = (CharacterData) child;
+			return cd.getData();
+		}
+		return "?";
+	}
 
 }
