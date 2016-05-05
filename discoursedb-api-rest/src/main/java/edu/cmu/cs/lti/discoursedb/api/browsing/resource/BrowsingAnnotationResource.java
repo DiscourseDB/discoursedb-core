39bb60ffa28d10019582d96e82d14aa11712fdd5
Chris Bogart <cbogartdenver@gmail.com>
2016-05-05 10:39:38 -0400

    Add character range to Annotations (and use functional style code)

--- discoursedb-api-rest/src/main/java/edu/cmu/cs/lti/discoursedb/api/browsing/resource/BrowsingAnnotationResource.java
+++ discoursedb-api-rest/src/main/java/edu/cmu/cs/lti/discoursedb/api/browsing/resource/BrowsingAnnotationResource.java
@@ -1,31 +1,22 @@
 package edu.cmu.cs.lti.discoursedb.api.browsing.resource;
 
-import java.util.Date;
-import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.springframework.hateoas.ResourceSupport;
-import org.springframework.hateoas.Resources;
 
 import edu.cmu.cs.lti.discoursedb.core.model.annotation.AnnotationInstance;
-import edu.cmu.cs.lti.discoursedb.core.model.annotation.Feature;
-import edu.cmu.cs.lti.discoursedb.core.model.macro.DiscoursePart;
 
 public class BrowsingAnnotationResource extends ResourceSupport {
 	private String type;
+	private String range = "";
 	private List<BrowsingFeatureResource> features;
 	
 	
 	public BrowsingAnnotationResource(AnnotationInstance ai) {
 		this.setType(ai.getType());
-		List feats = new LinkedList<BrowsingFeatureResource>();
-		for (Feature f: ai.getFeatures()) {
-			feats.add(new BrowsingFeatureResource(f));
-		}
-		this.setFeatures(feats);
+		this.setRange(ai.getBeginOffset() + "-" + ai.getEndOffset());
+		this.setFeatures(ai.getFeatures().stream().map(f -> new BrowsingFeatureResource(f)).collect(Collectors.toList()));
 	}
 
 
@@ -46,6 +37,16 @@ public class BrowsingAnnotationResource extends ResourceSupport {
 
 	public void setType(String type) {
 		this.type = type;
+	}
+
+
+	public String getRange() {
+		return range;
+	}
+
+
+	public void setRange(String range) {
+		this.range = range;
 	}
 
 }
