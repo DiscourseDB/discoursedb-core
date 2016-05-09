88f91df5c5276f4d9c6591643c5f339cdda336dc
Chris Bogart <cbogartdenver@gmail.com>
2016-05-09 13:38:21 -0400

    Exclude autoconfiguring parts of io.brat that we don't need

--- discoursedb-api-rest/src/main/java/edu/cmu/cs/lti/discoursedb/api/DiscourseApiStarter.java
+++ discoursedb-api-rest/src/main/java/edu/cmu/cs/lti/discoursedb/api/DiscourseApiStarter.java
@@ -4,9 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.springframework.hateoas.config.EnableEntityLinks;
 import org.springframework.scheduling.annotation.EnableAsync;
+
+import edu.cmu.cs.lti.discoursedb.annotation.brat.io.BratExport;
+import edu.cmu.cs.lti.discoursedb.annotation.brat.io.BratImport;
+import edu.cmu.cs.lti.discoursedb.annotation.brat.io.BratService;
+import edu.cmu.cs.lti.discoursedb.configuration.BaseConfiguration;
 
 /**
  * A SpringBootApplication that launches a server that hosts the API.
@@ -16,7 +22,8 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableEntityLinks
 @EnableAsync
 @EntityScan(basePackageClasses = { DiscourseApiStarter.class, Jsr310JpaConverters.class })
-@ComponentScan(value = { "edu.cmu.cs.lti.discoursedb" })
+@ComponentScan(value = { "edu.cmu.cs.lti.discoursedb"},  excludeFilters = {
+				@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = { BratExport.class, BratImport.class }) })
 public class DiscourseApiStarter {
 
 	public static void main(String[] args) {
