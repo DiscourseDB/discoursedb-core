bed11152b2035871808a08b85344ef64ef34acbd
Oliver Ferschke <oliver.ferschke@gmail.com>
2016-05-15 13:14:52 -0400

    Update package-info.java

--- discoursedb-annotation-io-lightside/src/main/java/edu/cmu/cs/lti/discoursedb/annotation/lightside/package-info.java
+++ discoursedb-annotation-io-lightside/src/main/java/edu/cmu/cs/lti/discoursedb/annotation/lightside/package-info.java
@@ -2,7 +2,7 @@
  * This module contains services for exporting annotations from DiscourseDB to
  * <a href="http://ankara.lti.cs.cmu.edu/side/">LightSide</a> to train ML
  * classifiers and for applying LightSide models to content from DiscourseDB to
- * generate annotations.
+ * generate and import annotations.
  * 
  * @author Oliver Ferschke
  *
