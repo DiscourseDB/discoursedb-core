b1c5456de38a1f5108766b7e7cd6beab8b8d8bc3
Chris Bogart <cbogartdenver@gmail.com>
2016-05-24 11:47:31 -0400

    Find all discourseparts that a user has contributed to

--- discoursedb-model/src/main/java/edu/cmu/cs/lti/discoursedb/core/repository/macro/DiscoursePartRepository.java
+++ discoursedb-model/src/main/java/edu/cmu/cs/lti/discoursedb/core/repository/macro/DiscoursePartRepository.java
@@ -1,31 +1,15 @@
 package edu.cmu.cs.lti.discoursedb.core.repository.macro;
 
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RestResource;
-import org.springframework.hateoas.Resources;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.mysema.query.types.expr.BooleanExpression;
+import java.util.Set;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
 import edu.cmu.cs.lti.discoursedb.core.model.macro.DiscoursePart;
+import edu.cmu.cs.lti.discoursedb.core.model.user.User;
 import edu.cmu.cs.lti.discoursedb.core.repository.BaseRepository;
 
 public interface DiscoursePartRepository extends BaseRepository<DiscoursePart,Long>{
@@ -60,5 +44,25 @@ public interface DiscoursePartRepository extends BaseRepository<DiscoursePart,Lo
 	Page<DiscoursePart> findAllNotAnnotatedWithTypePaged(@Param("annotationType") String type, Pageable pageable);
 
 	@Query("select type, count(*) as count from DiscoursePart dp group by type")
-	List<Object[]> countsByType();
+	List<Object[]> countsByType();	
+	
+	/*@Query("select dp1 from User u "
+			+ "left join Content conte on conte.user=u "
+			+ "left join Contribution contr on contr.firstRevision = conte "
+			+ "left join contr.contributionPartOfDiscourseParts cpdp  "
+			+ "left join cpdp.discoursePart dp1 "
+			+ "where u=:u")*/
+	@Query("select dp1 from DiscoursePartContribution cpdp "
+			+ "inner join cpdp.contribution contr  "
+			+ "inner join contr.firstRevision conte "
+			+ "left join cpdp.discoursePart dp1 "
+			+ "where conte.author=?1")
+	Set<DiscoursePart> findAllThatIncludesUser(User u);
+	
+	@Query("select dp1 from DiscoursePartContribution cpdp "
+			+ "inner join cpdp.contribution contr  "
+			+ "inner join contr.firstRevision conte "
+			+ "left join cpdp.discoursePart dp1 "
+			+ "where conte.author=?1")
+	Page<DiscoursePart> findAllThatIncludesUser(User u, Pageable p);
 }
