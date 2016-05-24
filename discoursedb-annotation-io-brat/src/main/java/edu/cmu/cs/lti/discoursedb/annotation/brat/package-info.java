fe736db0d2eb98a9850520b5437576906360202d
Chris Bogart <cbogartdenver@gmail.com>
2016-05-24 15:24:32 -0400

    Merged changes from Oliver

--- discoursedb-annotation-io-brat/src/main/java/edu/cmu/cs/lti/discoursedb/annotation/brat/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * This module contains services for exporting annotations from DiscourseDB to <a href="http://brat.nlplab.org">BRAT</a> and for re-importing BRAT annotations into DiscourseDB.
- * 
- * @author Oliver Ferschke
- *
- */
-package edu.cmu.cs.lti.discoursedb.annotation.brat;
