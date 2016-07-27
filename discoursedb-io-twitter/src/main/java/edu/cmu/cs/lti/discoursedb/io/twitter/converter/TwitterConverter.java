61478e594a2faaa78c5c4be760124e2af97f869d
ferschke <ferschke@cs.cmu.edu>
2016-07-27 14:43:02 -0400

    Moved mongo access to TwitterConverter class

--- discoursedb-io-twitter/src/main/java/edu/cmu/cs/lti/discoursedb/io/twitter/converter/TwitterConverter.java
+++ discoursedb-io-twitter/src/main/java/edu/cmu/cs/lti/discoursedb/io/twitter/converter/TwitterConverter.java
@@ -1,15 +1,26 @@
 package edu.cmu.cs.lti.discoursedb.io.twitter.converter;
 
-import java.io.File;
 import java.text.ParseException;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
 
+import lombok.extern.log4j.Log4j;
+import twitter4j.Status;
+import twitter4j.TwitterException;
+import twitter4j.TwitterObjectFactory;
+
+
+@Log4j
 @Component
 public class TwitterConverter implements CommandLineRunner {
 	
@@ -22,28 +33,38 @@ public class TwitterConverter implements CommandLineRunner {
 	public void run(String... args) throws ParseException {
 		
 		if (args.length != 3) {
-			logger.error("Usage: TwitterConverterApplication <DiscourseName> <DataSetName> <TwitterDumpPath>");
+			logger.error("Usage: TwitterConverterApplication <DiscourseName> <DataSetName> <MongoDatabaseName> <MongoCollectionName");
 			return;
 		}
 		
 		String discourseName = args[0];
 		String datasetName = args[1];		
-		String filePath = args[2];
-		
-		//check whether the given file exists
-		File f = new File(filePath);
-		if (!f.exists() || !f.isDirectory() || !f.canRead()) {
-			logger.error("Input path is not a directory, does not exist or is not readable.");
-			return;
-		}
+		String dbName = args[2];
+		String collectionName = args[3];		
 		
 		logger.info("Starting Twitter data conversion");
-		this.convert(discourseName, datasetName, filePath);
+		this.convert(discourseName, datasetName, dbName, collectionName);
 		logger.info("Twitter data conversion completed");
 	}
 	
-	private void convert(String discourseName, String datasetName, String filePath) throws ParseException {
+	private void convert(String discourseName, String datasetName, String dbName, String collectionName) {
 		
+		MongoClient mongoClient = new MongoClient();
+		MongoDatabase db = mongoClient.getDatabase(dbName);
+		MongoCollection<Document> col = db.getCollection(collectionName);
+		
+		MongoCursor<Document> it = col.find().iterator();
+		while(it.hasNext()){
+			Document curdoc = it.next();
+			try{
+				Status curStatus = TwitterObjectFactory.createStatus(curdoc.toJson());
+				System.out.println(curStatus.getCreatedAt());
+				//TODO process curStatus
+			}catch(TwitterException e){
+				log.warn("Could not parse tweet",e);
+			}
+		}
+		mongoClient.close();
 		
 	}
 
