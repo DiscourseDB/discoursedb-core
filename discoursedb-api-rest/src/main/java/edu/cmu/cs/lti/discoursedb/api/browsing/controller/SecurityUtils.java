4b5a7b50652bbc55df79875e351c428fa47bc11c
Chris Bogart <cbogartdenver@gmail.com>
2017-04-24 16:04:51 -0400

    Discourse failure check

--- discoursedb-api-rest/src/main/java/edu/cmu/cs/lti/discoursedb/api/browsing/controller/SecurityUtils.java
+++ discoursedb-api-rest/src/main/java/edu/cmu/cs/lti/discoursedb/api/browsing/controller/SecurityUtils.java
@@ -149,6 +149,7 @@ public class SecurityUtils {
 		}
 	}
 	public void authorizedDiscourseFail() {
+		logger.info("No discourse identified -- so authorization fails");
 		throw new BrowsingRestController.UnauthorizedDiscourseAccess();
 	}
 	 
